ce7a215d0f303ee443cf1dfd0dc1cd9c27d36a89
hanyaguang <13649248525@163.com>
2022-02-12 20:37:29 +0800

    admin服务调试运行成功

--- cdf-admin/src/main/java/com/cdf/mall/CdfAdminApplication.java
+++ cdf-admin/src/main/java/com/cdf/mall/CdfAdminApplication.java
@@ -1,9 +1,14 @@
 package com.cdf.mall;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class CdfAdminApplication {
 
     public static void main(String[] args) {
