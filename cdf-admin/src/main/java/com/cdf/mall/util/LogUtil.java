ab018a690ea0dfddf3fc7ef596f056c4c347296e
hanyaguang <13649248525@163.com>
2022-04-21 09:30:48 +0800

    测试提交

--- cdf-admin/src/main/java/com/cdf/mall/util/LogUtil.java
+++ cdf-admin/src/main/java/com/cdf/mall/util/LogUtil.java
@@ -5,14 +5,14 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 /**
- * @Description TODO
+ * @Description 日志工具类
  * @Author hanyaguang
  * @Date 2022/4/19 15:42
  * @Version 1.0
  */
 public class LogUtil<T> {
     /**
-     * 对象比较器
+     * 对象比较器  比较两个对象中的任一字段是否相同。
      * 比较结果eg：1、字段名称loginName,旧值:liu,新值:gu;2、字段名称address,旧值:hunan,新值:neimenggu
      * @param oldBean
      * @param newBean
